faa2e9139742b251b4f77d63d71aabcf0b93a176
rathsngt <62255740+rathsngt@users.noreply.github.com>
2020-07-20 17:10:09 +0700

    Add all files

---
+++ converter.js
@@ -0,0 +1,41 @@
+function weightConverter(source,valNum) {
+    valNum = parseFloat(valNum);
+    var inputSqm = document.getElementById("inputSqm");
+    var inputRai = document.getElementById("inputRai");
+    var inputNgan = document.getElementById("inputNgan");
+    var inputWah = document.getElementById("inputWah");
+    var inputTotalWah = document.getElementById("inputTotalWah");
+
+    if (source == "inputSqm") {
+        inputTotalWah.value = (valNum/4).toFixed(2)
+        inputRai.value = Math.floor(valNum/1600)
+        valNum = valNum - (inputRai.value*1600)
+        inputNgan.value = Math.floor(valNum/400)
+        valNum = valNum - (inputNgan.value*400)
+        inputWah.value = (valNum/4).toFixed(2)
+    }
+
+    if (source == "inputRai") {
+        inputTotalWah.value = (valNum*400) + (inputNgan.value*100) + (inputWah.value*1)
+        inputSqm.value = (inputTotalWah.value*4)
+    }
+
+    if (source == "inputNgan") {
+        inputTotalWah.value = (inputRai.value*400) + (valNum*100) + (inputWah.value*1)
+        inputSqm.value = (inputTotalWah.value*4)
+    }
+
+    if (source == "inputWah") {
+        inputTotalWah.value = (inputRai.value*400) + (inputNgan.value*100) + (valNum*1)
+        inputSqm.value = (inputTotalWah.value*4)
+    }
+
+    if (source == "inputTotalWah") {
+        inputSqm.value = (valNum*4).toFixed(2)
+        inputRai.value = Math.floor(valNum/400)
+        valNum = valNum - (inputRai.value*400)
+        inputNgan.value = Math.floor(valNum/100)
+        valNum = valNum - (inputNgan.value*100)
+        inputWah.value = (valNum).toFixed(2)
+    }
+}
